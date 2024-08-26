9e7eeb863d8bea57800ef1fe6938eaaf318dfc90
potteru <potteru2k11@gmail.com>
2024-08-26 16:53:49 +0000

    firast commit

---
+++ src/components/Tasks/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {TaskName, TagsCategory, ListItem} from './style'
+
+const Tasks = props => {
+  const {taskDetails} = props
+  const {taskName, taskCategory} = taskDetails
+
+  return (
+    <ListItem>
+      <TaskName>{taskName}</TaskName>
+      <TagsCategory>{taskCategory}</TagsCategory>
+    </ListItem>
+  )
+}
+
+export default Tasks
